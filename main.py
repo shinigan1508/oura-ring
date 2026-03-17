3ae616ec159ef744490d34c7e81cb554e46d0a8e
shinigan1508 <shinmaksim2002@gmail.com>
2026-03-17 17:12:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
+import asyncio
 from telegram import Bot
 
-print("STARTING BOT...")
+async def main():
+    print("STARTING BOT...")
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
+    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+    CHAT_ID = os.environ.get("CHAT_ID")
 
-print("TOKEN:", TELEGRAM_TOKEN)
-print("CHAT_ID:", CHAT_ID)
+    print("TOKEN:", TELEGRAM_TOKEN)
+    print("CHAT_ID:", CHAT_ID)
 
-bot = Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
 
-bot.send_message(chat_id=CHAT_ID, text="Бот работает 🚀")
+    await bot.send_message(chat_id=CHAT_ID, text="Бот работает 🚀")
 
-print("MESSAGE SENT")
+    print("MESSAGE SENT")
+
+asyncio.run(main())
