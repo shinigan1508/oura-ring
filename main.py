0258a87b553e41e87dd0523ef60a87e28ae94d0f
shinigan1508 <shinmaksim2002@gmail.com>
2026-03-17 17:10:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Bot started successfully 🚀")
+import os
+from telegram import Bot
+
+print("STARTING BOT...")
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+print("TOKEN:", TELEGRAM_TOKEN)
+print("CHAT_ID:", CHAT_ID)
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+bot.send_message(chat_id=CHAT_ID, text="Бот работает 🚀")
+
+print("MESSAGE SENT")
